a967d3cdf9dfc22bece7d44bbd6fb2aa87dbabb8
nyanko <nyanko@nyankosoft.com>
2018-04-23 01:07:01 +0900

    Writing the proxy lookup script. Work in progress.

--- windows/get_proxy_url.bat
+++ windows/get_proxy_url.bat
@@ -4,14 +4,20 @@
 :: - TrifleJS (confirmed with v0.4)
 :: - curl
 
-:: Usage
+:: Usage:
+::
+:: get_proxy_url.bat nyankosoft.space
+:: -> Prints out the URL of the proxy to be used to access nyankosoft.space to stdout
+::
 :: To call this batch file in your batch file and store the proxy URL printed to stdout to a variable,
 :: copy and paste the line below:
 ::
-:: for /f "delims=" %%A in ('get_proxy_url.bat') do set "var=%%A"
+:: for /f "delims=" %%A in ('get_proxy_url.bat github.com') do set "var=%%A"
 ::
 
 @echo off
+
+setlocal EnableExtensions EnableDelayedExpansion
 
 set triflejs_path="%~dp0\TrifleJS_v0.4\TrifleJS.exe"
 set curl_path="%~dp0\curl-7.59.0\src\curl.exe"
@@ -23,17 +29,18 @@ set target_url=%1
 :: These are not necessary unless PAC is enabled, but writing these lines in the else block causes
 :: some obscure errors so it's done here.
 :: TODO: add definitions of other functions.
-echo function shExpMatch(url, pattern) {pattern = pattern.replace(/\./g, '\\.'); pattern = pattern.replace(/\*/g, $
+echo function shExpMatch(url, pattern) {pattern = pattern.replace(/\./g, '\\.'); pattern = pattern.replace(/\*/g, '.*'); pattern = pattern.replace(/\?/g, '.'); var newRe = new RegExp('^'+pattern+'$'); return newRe.test(url);} > pac.js
 echo console.log(FindProxyForURL('', >> pac.js
 echo '%target_url%' >> pac.js
 echo ));phantom.exit(); >> pac.js
 
+set reg_internet_settings="HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings"
 
 :: 1. Check if proxy auto-configuration is set up
 
 :: 1.1 Get the URL of the proxy auto-config script
 :: 1.1.1 Look up the registry key that has autoconfig URL and save the key and the value it to a text file.
-reg query "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v AutoConfigURL > pac_s$
+reg query %reg_internet_settings% /v AutoConfigURL > pac_script_url.txt 2>&1
 
 :: 1.1.2 Extract the third line from the text file.
 ::     The third line has the proxy URL and it typically looks like this:
@@ -45,30 +52,35 @@ for /f "usebackq delims=" %%a in (`more +%lineNr% pac_script_url.txt`) DO (
   break
 )
 
-if "%auto_config_url_line:~0,5%" == "ERROR" (
-    echo PAC script not detected
+if "%auto_config_url_line:~4,13%"=="AutoConfigURL" (
+    echo PAC script detected
 
-    reg query "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Internet Settings" | find /i "proxyserv$
-) else (
-echo PAC script detected
-
-set ac_url=%auto_config_url_line:~31%
-%curl_path% -s -o script.pac %ac_url%
+    set pac_url=%auto_config_url_line:~31%
+    echo PAC script URL: %pac_url%
+    %curl_path% -s -o script.pac %pac_url%
 
 REM 1.2. Create a JS script from the PAC script
 REM 1.2.1 Define the pac script function(s)
 
 REM 1.2.2 Append the PAC script
-type script.pac >> pac.js
+    type script.pac >> pac.js
 
 REM 1.2.3 Append the main function call.
 
 REM Print the proxy URL to stdout
-%triflejs_path% pac.js
+    %triflejs_path% pac.js
+)
+
+if not "%auto_config_url_line:~4,13%"=="AutoConfigURL" (
+    echo PAC script not detected
+
+    reg query %reg_internet_settings% /v ProxyEnable
+
+REM reg query %reg_internet_settings% | find /i "proxyserver"
 )
 
 REM Clean up; delete temporary files.
-del script.pac
-del pac_script_url.txt
-del pac.js
+:: del script.pac
+:: del pac_script_url.txt
+:: del pac.js
 
